a99ed7549a9ab749c170da0a5bb325f80a363b81
Metalzee <sacha.vekemanfb@gmail.com>
2024-12-18 13:52:46 +0100

    add of player rotation

--- Assets/Scripts/BulletSpawn.cs
+++ Assets/Scripts/BulletSpawn.cs
@@ -7,44 +7,35 @@ public class BulletSpawn : MonoBehaviour
     public Transform SpawnPoint;
     public GameObject[] Bullets;
     public int WhichBullet;
-
-    public int maxAmmo = 5;
-    private int currentAmmo;
-    public float reloadTime = 2f;
-    private float reloadTimer;
-
+    public float FireRate = 0.5f;
+    
     private bool canShoot = true;
 
     void Start()
     {
-        currentAmmo = maxAmmo;
-        reloadTimer = 0f;
     }
 
     void Update()
     {
-        if (currentAmmo < maxAmmo)
-        {
-            reloadTimer += Time.deltaTime;
-            if (reloadTimer >= reloadTime)
-            {
-                currentAmmo++;
-                reloadTimer = 0f;
-            }
-        }
     }
 
     void OnJump()
     {
-        if (canShoot && currentAmmo > 0)
+        if (canShoot)
         {
-            Instantiate(Bullets[WhichBullet], SpawnPoint.position, SpawnPoint.rotation);
-            currentAmmo--;
-
-            if (currentAmmo <= 0)
-            {
-                canShoot = false;
-            }
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(Bullets[WhichBullet], SpawnPoint.position, SpawnPoint.rotation);
+        canShoot = false;
+        Invoke(nameof(ResetShoot), FireRate);
+    }
+
+    private void ResetShoot()
+    {
+        canShoot = true;
     }
 }
